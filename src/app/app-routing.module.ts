e637bcafbbe32133d4319213c0239b6e368bd055
hengstchon <hengstchon@gmail.com>
2024-11-05 10:21:39 +0100

    feat: add basic router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'watchlist',
+    loadComponent: () =>
+      import('./components/watchlist/watchlist.component').then(
+        (m) => m.WatchlistComponent,
+      ),
+  },
+  {
+    path: 'movie/:id',
+    loadComponent: () =>
+      import('./components/movie-detail/movie-detail.component').then(
+        (m) => m.MovieDetailComponent,
+      ),
+  },
+];
