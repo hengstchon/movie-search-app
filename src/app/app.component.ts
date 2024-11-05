cd9ac502967f6fab08324bdd63ae56d3fbd9057a
hengstchon <hengstchon@gmail.com>
2024-11-05 11:43:45 +0100

    refactor: remove unrelated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'movie-search-app';
